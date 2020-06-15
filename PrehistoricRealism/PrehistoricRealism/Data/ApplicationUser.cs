118ecdc4bd4a832bd10e753f6871edfd3393ba4f
Tanner Percival <percivaltanner@gmail.com>
2020-06-15 10:30:27 -0700

    setup for recode

---
+++ PrehistoricRealism/PrehistoricRealism/Data/ApplicationUser.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrehistoricRealism.Data
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Birthday { get; set; }
+        public string PrStaff { get; set; } = "false";
+    }
+
+
+    public static class ApplicationRoles
+    {
+        public const string Member = "Member";
+
+        public const string Admin = "ADMIN";
+
+
+    }
+}
